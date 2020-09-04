51fe56599e630412d58d866a9a34ecc1184c8dff
Ibrahim Karmadzha <ibrahimkarmadzha@Ibrahims-MacBook-Pro.local>
2020-09-04 15:52:35 -0700

    Updates

---
+++ precache-manifest.f011ed6b2a0fa7ca4928e0612f652f8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8eb5c877f6d9732757c7c4c03f361630",
+    "url": "/photo-app/index.html"
+  },
+  {
+    "revision": "f468ff5081c1871ddfe0",
+    "url": "/photo-app/static/css/main.c5c2ca5a.chunk.css"
+  },
+  {
+    "revision": "fcbba96e88503ccbe269",
+    "url": "/photo-app/static/js/2.dddf36b7.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/photo-app/static/js/2.dddf36b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f468ff5081c1871ddfe0",
+    "url": "/photo-app/static/js/main.889811d0.chunk.js"
+  },
+  {
+    "revision": "06f827362294a954fde1",
+    "url": "/photo-app/static/js/runtime-main.efc72e9c.js"
+  }
+]);
